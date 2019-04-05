0ef1ecce6e9eb030e6c2683dc93debbbdbef0a86
Chris Grabosky <chris@grabosky.net>
2019-04-05 09:26:19 -0400

    bug fixes

--- BashScripts/run.sh
+++ BashScripts/run.sh
@@ -1,25 +1,34 @@
 #!/bin/bash
-
+  
 while [ 1 ] 
 do
         #1 packets transmitted, 1 received, 0% packet loss, time 0ms
         pr=`ping google.com -c 1 | tail -n2 | head -n 1`
-        trans=`echo $pr | cut -d" " -f1`
-        recv=`echo $pr | cut -d" " -f4`
-        t=`echo $pr | cut -d" " -f10|cut -d"m" -f 1`
         d=`date +'%Y-%m-%dT%R:%S.%N'`
-        isdown='false'
+        hasFailed=0
 
-        if [ $recv -eq 0 ]
+        if [[ ${#pr} -lt 5 ]]
         then
-                isdown='true'
+                json="{'trans':1, 'recv':0, 'dnsfail':true, 'clidate':'$d', 'isdown':true}"
+                hasFailed=1
+        else
+                trans=`echo $pr | cut -d" " -f1`
+                recv=`echo $pr | cut -d" " -f4`
+                t=`echo $pr | cut -d" " -f10|cut -d"m" -f 1`
+                isdown='false'
+
+                if [ $recv -eq 0 ]
+                then
+                        isdown='true'
+                        hasFailed=1
+                fi
+                json="{'trans':$trans, 'recv':$recv, 'latency':$t, 'clidate':'$d', 'isdown':$isdown, 'dnsfail':false}"
         fi
 
-        json="{'trans':$trans, 'recv':$recv, 'latency':$t, 'clidate':'$d', 'isdown':$isdown}"
         json=`echo $json | sed "s/'/\"/g"`
         echo $json>>/home/ubuntu/iscomcastdown.txt
 
-        if [ $recv -eq 0 ]
+        if [ $hasFailed -eq 1 ]
         then
                 echo $json>>/home/ubuntu/iscomcastdown_yes.txt
         else
